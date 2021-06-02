37db40969c1b4b7dc33ebbf9ab3171c8a40453fe
Dmitriy-max <84131104+Dmitriy-max@users.noreply.github.com>
2021-06-02 05:35:05 +0000

    Add files via upload

---
+++ 01-06-2021-003-раздел строки по запятой.py
@@ -0,0 +1,16 @@
+S = input('Enter any sentence with comma: ')
+S = S.index(',')
+A = S+1
+print('Comma is found at position:',A)
+print('Thanks for using our application')
+print('(C) 2021 Dmitry Detukov')
+
+#Ожидаемый результат работы программы:
+
+#Enter any sentence with comma> Ivan,Sidorov
+
+#Comma is found at position: 5
+
+
+#Thanks for using our application
+
