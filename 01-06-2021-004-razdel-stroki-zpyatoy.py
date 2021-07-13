a0faeb1eb8e09dcada0cfa96832c934b57613e68
Dmitriy-max <84131104+Dmitriy-max@users.noreply.github.com>
2021-07-13 08:23:05 +0000

    Add files via upload

---
+++ 01-06-2021-004-razdel-stroki-zpyatoy.py
@@ -0,0 +1,35 @@
+# Получаем данные от пользователя
+sentence = input('Enter your name and surname> ')
+print()
+# Разбиваем предложение на массив по запятую.
+words = sentence.split(',')
+# print(words)
+sentenceLen = len(sentence)
+# print(wordsLen)
+if sentenceLen == 0:
+    print('Please, Enter your name and surname. Try again...')
+    exit(1) 
+for i in range (sentenceLen):
+        if sentence[i].isdigit():
+                print('Please, Enter your name and surname. Try again...')
+                exit(1) 
+
+print(words[0]) # выводим первое слово
+print(words[1]) # выводим второе слово
+print()
+print()
+print('Thanks for using our application')
+print('(c) 2021 Dmitry Detukov')
+
+# Ожмдаемое:
+#Enter your name and surname> Ivan,Sidorov
+
+#Ivan
+#Sidorov
+
+
+#Thanks for using our application
+#(c) YEAR Name Surname
+
+
+
