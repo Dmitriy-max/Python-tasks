a98f400dab60726f13aaf69dfaffc465b85d5093
Dmitriy-max <84131104+Dmitriy-max@users.noreply.github.com>
2021-09-05 12:29:52 +0000

    Update 01-06-2021-006c-вложенный-цикл.py

--- 01-06-2021-006c-вложенный-цикл.py
+++ 01-06-2021-006c-вложенный-цикл.py
@@ -11,21 +11,21 @@ numberLen2 = len(number2)
 if numberLen1 == 0 or numberLen2 == 0:
     print('Input numbers. Please, try again...')
     exit(1) 
-# Если пользователь ввел буквенное значение выходим из программы с кодом 1.
+# Если пользователь ввел буквенное значение выходим из программы с кодом 2.
 if  type(number1) != int or type(number2) != int:
     try:
         number1 = int(number1)
 
     except ValueError:
         print("Incorrect input. Please input only numbers, not words. Try again...")
-        exit(1)
-# Если пользователь ввел отрицательные числа выходим из программы с кодом 1.
+        exit(2)
+# Если пользователь ввел отрицательные числа выходим из программы с кодом 3.
 numberInt1 = int(number1)
 numberInt2 = int(number2)
 
 if numberInt1 < 0 or numberInt2 < 0:
     print('Please, input positive number. Try again...')
-    exit(1)
+    exit(3)
 # Создаем вложенный цикл и выводим данные на экран 
 for i in range (0,numberInt1+1):
         for j in range (0, numberInt2+1):
