140badb55804f5b031750333c142e8c7c333179b
Dmitry Detukov <84131104+thedetukov@users.noreply.github.com>
2022-08-30 17:30:40 +0000

    Update fibo-number.py

--- fibo-number.py
+++ fibo-number.py
@@ -1,6 +1,6 @@
 def chekNumber():
-    number = int(input('Enter a number from 0 to 9999> ') )
-    fiboRow = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765 ] 
+    number = int(input('Enter a number from 0 to 9999> '))
+    fiboRow = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765] 
     if number in fiboRow:
         print('YES')
     else:
